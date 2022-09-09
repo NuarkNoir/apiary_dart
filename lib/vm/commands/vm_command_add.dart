0ee7852b05fef43b1ebf14309cc4db77aa0b947d
Andrew nuark G <me@nuark.xyz>
2022-09-10 00:49:48 +0700

    add command add

---
+++ lib/vm/commands/vm_command_add.dart
@@ -0,0 +1,15 @@
+import 'package:apiary_dart/entities/base_entity.dart';
+
+import '../vm.dart';
+import 'vm_command.dart';
+
+class VMCommandAdd extends VMCommand {
+  final BaseEntity _entity;
+
+  VMCommandAdd(this._entity);
+
+  @override
+  Future<void> execute(VMContext ctx) async {
+    ctx.entities.add(_entity);
+  }
+}
