70cd5c8bf50d110c4a506da27eb44c4cd174ff29
Andrew nuark G <me@nuark.xyz>
2022-09-09 23:39:39 +0700

    scaffolded project

---
+++ bin/apiary_dart.dart
@@ -0,0 +1,3 @@
+import 'package:apiary_dart/apiary_dart.dart' as apiary_dart;
+
+void main(List<String> arguments) async {}
---
+++ test/apiary_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:apiary_dart/apiary_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
