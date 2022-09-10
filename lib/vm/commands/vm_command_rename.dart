9680b7d6aa71a4e13a3825134ca4f06986eb6fa2
Andrew nuark G <me@nuark.xyz>
2022-09-10 12:57:11 +0700

    did not add rename comand, but rem instead, fixing

---
+++ lib/vm/commands/vm_command_rename.dart
@@ -0,0 +1,14 @@
+import '../vm.dart';
+import 'vm_command.dart';
+
+class VMCommandRename implements VMCommand {
+  final int _index;
+  final String _newName;
+
+  const VMCommandRename(this._index, this._newName);
+
+  @override
+  Future<void> execute(VMContext ctx) async {
+    ctx.entities.get(_index).name = _newName;
+  }
+}
