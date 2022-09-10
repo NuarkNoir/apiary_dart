44d775ef5cfb0d7d41629c3b6809466ca8b024bd
Andrew nuark G <me@nuark.xyz>
2022-09-11 00:56:11 +0700

    Message, when list empty

--- lib/vm/commands/vm_command_print.dart
+++ lib/vm/commands/vm_command_print.dart
@@ -7,9 +7,13 @@ class VMComandPrint implements VMCommand {
   @override
   Future<void> execute(VMContext ctx) async {
     stdout.writeln("Count of elements: ${ctx.entities.length}");
-    for (int i = 0; i < ctx.entities.length; i++) {
-      final ent = ctx.entities.get(i);
-      stdout.writeln("$i. ${ent?.toString() ?? "[null element at $i]"}");
+    if (ctx.entities.length > 0) {
+      for (int i = 0; i < ctx.entities.length; i++) {
+        final ent = ctx.entities.get(i);
+        stdout.writeln("$i. ${ent?.toString() ?? "[null element at $i]"}");
+      }
+    } else {
+      stdout.writeln("No elements");
     }
   }
 }
