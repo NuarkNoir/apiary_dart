049c4749f3b1f4b7b787640db9fd4e6a897c5869
Andrew nuark G <me@nuark.xyz>
2022-09-11 02:10:29 +0700

    add repl

--- bin/apiary_dart.dart
+++ bin/apiary_dart.dart
@@ -3,16 +3,44 @@ import 'dart:io';
 import 'package:apiary_dart/apiary_dart.dart' as apiary_dart;
 
 void main(List<String> arguments) async {
-  arguments = [r"D:\apiary_dart\tests.sota"];
   if (arguments.length > 1 ||
       (arguments.length == 1 && !File(arguments[0]).existsSync())) {
     print('Usage: apiary_dart [<script path>]');
     return;
   }
 
+  if (arguments.isEmpty) {
+    await runRepl();
+  } else {
+    await runFile(arguments[0]);
+  }
+}
+
+Future<void> runRepl() async {
+  final vmContext = apiary_dart.VMContext.createVMContext();
+  final parser = apiary_dart.Parser();
+
+  while (true) {
+    stdout.write("> ");
+    final line = stdin.readLineSync();
+    if (line == null) {
+      break;
+    }
+    parser.parse([line.trim()].where((element) => element.isNotEmpty));
+    final lexems = apiary_dart.Lexer.lex(parser.linesOfTokens);
+    for (final lexem in lexems) {
+      vmContext.pushCommand(lexem);
+    }
+    parser.linesOfTokens.clear();
+
+    await vmContext.executeCommands();
+  }
+}
+
+Future<void> runFile(String path) {
   final vmContext = apiary_dart.VMContext.createVMContext();
 
-  final script = File(arguments[0]).readAsStringSync();
+  final script = File(path).readAsStringSync();
   final parser = apiary_dart.Parser();
   parser.parse(script.split("\n").where((element) => element.isNotEmpty));
   final lexems = apiary_dart.Lexer.lex(parser.linesOfTokens);
@@ -20,5 +48,5 @@ void main(List<String> arguments) async {
     vmContext.pushCommand(lexem);
   }
 
-  vmContext.executeCommands();
+  return vmContext.executeCommands();
 }
